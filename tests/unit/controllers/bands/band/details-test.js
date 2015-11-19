ec0fd7431b2cc0d35bcb85bdef49f1d1199fbd1c
pathiggins13 <pat_higgins@hotmail.com>
2015-11-19 16:10:40 -0700

    finishes api requests

---
+++ tests/unit/controllers/bands/band/details-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:bands/band/deatils', 'Unit | Controller | bands/band/deatils', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
