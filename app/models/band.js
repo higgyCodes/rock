87924b5ea4e764b167061a4f69f0075894e96829
pathiggins13 <pat_higgins@hotmail.com>
2015-11-19 15:18:06 -0700

    adds api extension

--- app/models/band.js
+++ app/models/band.js
@@ -1,15 +1,8 @@
-export default Ember.Object.extend({ 
-	name: '',
-	description: '',
+//import Ember from 'ember';
+import DS from 'ember-data';
 
-
-	setupSongs: Ember.on('init', function(){
-		if(!this.get('songs')) {
-			this.set('songs', []);
-		}
-	}),
-
-	slug: Ember.computed('name', function() { 
-		return this.get('name').dasherize();
-	}), 
+export default DS.Model.extend({ 
+	name: 			DS.attr('string'),
+	description: 	DS.attr(),
+	songs: 			DS.hasMany('song')
 });
--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,68 +1,22 @@
 import Ember from 'ember';
-import Band from '../models/band';
-import Song from '../models/band';
-
-var blackDog = Song.create({ 
-	title: 'Black Dog',
-	band: 'Led Zeppelin', 
-	rating: 3
-});
-var yellowLedbetter = Song.create({ 
-	title: 'Yellow Ledbetter',
-	band: 'Pearl Jam',
-	rating: 4
-});
-var daughter = Song.create({ 
-	title: 'Daughter',
-	band: 'Pearl Jam', 
-	rating: 5
-});
-var pretender = Song.create({ 
-	title: 'The Pretender', 
-	band: 'Foo Fighters', 
-	rating: 2
-});
-
-var BandsCollection = Ember.Object.extend({
-	content: [],
-	sortProperties: ['name:desc'],
-	sortedContent: Ember.computed.sort('content', 'sortProperties'),
-});
-
-
-
-
-
-var ledZeppelin = Band.create({ 
-	name: 'Led Zeppelin', 
-	songs: [blackDog] }); 
-var pearlJam = Band.create({ 
-	name: 'Pearl Jam',
-	description: 'Pearl Jam is an American rock band from Seattle',
-	songs: [daughter, yellowLedbetter] });
-var fooFighters = Band.create({ 
-	name: 'Foo Fighters', 
-	songs: [pretender] });
-
-var bands = BandsCollection.create();
-bands.get('content'). pushObjects([ledZeppelin, pearlJam, fooFighters]);
 
 export default Ember.Route.extend({ 
-	model: function() {
-return bands; 
-},
-
-actions: {
-	didTransition: function() {
-		document.title = 'Bands - Rock & Roll';
+	model: function(params) {
+		return this.store.findAll('band');
 	},
-	createBand: function() {
-		var name = this.get('controller').get('name');
-		var band = Band.create({ name: name });
-		bands.get('content').pushObject(band);
-		this.get('controller').set('name', '');
-		this.transitionTo('bands.band.songs', band);
-	}
-}
+
+// actions: {
+// 	didTransition: function() {
+// 		document.title = 'Bands - Rock & Roll';
+// 	},
+// 	createBand: function() {
+// 		var name = this.get('controller').get('name');
+// 		var band = Band.create({ name: name });
+// 		bands.get('content').pushObject(band);
+// 		this.get('controller').set('name', '');
+// 		this.transitionTo('bands.band.songs', band);
+// 	}
+// }
+
 });
 
